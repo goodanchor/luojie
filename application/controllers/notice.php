25f563a3cd689eb341d0005729666151c61df5f5
anchor <932552863@qq.com>
2015-02-10 01:18:50 +0800

    公告后台部分

---
+++ application/controllers/notice.php
@@ -0,0 +1,83 @@
+<?php
+class Notice extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper(array('form','url'));
+        $this->load->model('notice_model');
+    }
+
+    function add_notice()
+    {
+        $session = $this->session->all_userdata();
+        $post = $this->input->post();
+        if(!isset($session['userid']) OR ($session['power']!=1)){
+            $res['msg'] = '您无权进行该操作';
+            $res['status'] = 0 ;
+        }
+        else if(!$post['title'] OR strlen($post['title'])>20){
+            $res['msg'] = '请输入的公告标题或标题过长';
+            $res['status'] = 0;
+        }
+        else if(!post['content']) {
+            $res['msg'] = '请输入公告内容';
+            $res['status'] = 0;
+        }
+        else if ($this->notice_model->add($post)){
+            $res['status'] = 1;
+            $res['msg'] = '添加文章成功';
+        }
+        else {
+            $res['status'] = 0;
+            $res['msg'] = '添加文章失败';            
+        }
+        echo json_encode($res);
+    }
+
+    function del_passage()
+    {
+        $session = $this->session->all_userdata();
+        $post = $this->input->post();
+        if(!isset($session['userid']) OR ($session['power']!=1)){
+            $res['msg'] = '您无权进行该操作';
+            $res['status'] = 0 ;
+        }
+        else if ($this->notice_model->delete($post)){
+            $res['msg'] = '删除成功';
+            $res['status'] = 1;
+        }
+        else {
+            $res['msg'] = '删除失败';
+            $res['status'] = 0;
+        }
+        echo json_encode($res);
+    }
+
+    function update_passage()
+    {
+        $session = $this->session->all_userdata();
+        $post = $this->input->post();
+        if(!isset($session['userid'])  OR ($session['power']!=1)){
+            $res['msg'] = '您无权进行该操作';
+            $res['status'] = 0 ;
+        }
+        else if(!$post['title'] OR strlen($post['title'])>20){
+            $res['msg'] = '请输入公告标题或标题过长';
+            $res['status'] = 0;
+        }
+        else if(!post['content']) {
+            $res['msg'] = '请输入公告内容';
+            $res['status'] = 0;
+        }
+        else if ($this->notice_model->update($post)){
+            $res['status'] = 1;
+            $res['msg'] = '修改公告成功';
+        }
+        else {
+            $res['status'] = 0;
+            $res['msg'] = '修改公告失败';            
+        }
+        echo json_encode($res);
+    }
+}
