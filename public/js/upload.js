ddf5c7afd5f7613c19ca483fa8568c3a9d5ed991
wsxxdwd <wsxxdwdd@gmail.com>
2015-02-10 13:40:49 +0800

    文件上传对接

--- public/js/upload.js
+++ public/js/upload.js
@@ -5,6 +5,13 @@ $(document).ready(function(){
 	        width         : 120,
 	        swf           : URL + 'public/uploadify.swf',
 	        uploader      : URL + 'index.php/files/upload',
+	        formData      : formData,
+	        'onUploadComplete' : function(file) {
+            	alert('文件 ' + file.name + ' 上传成功.');
+        	},
+        	'onUploadError' : function(file, errorCode, errorMsg, errorString) {
+	            alert('文件 ' + file.name + ' 上传失败: ' + errorString);
+	        }
 	    });
 	  },100);
 });
