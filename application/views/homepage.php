d8601efeaeb52bde47b23eccda4482c393b66c14
anchor <932552863@qq.com>
2015-02-11 16:35:05 +0800

    修复前端页面css展示问题

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -4,15 +4,14 @@
 <meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
 <base href = "<?php echo base_url();?>"/>
 <script type="text/javascript"> window.URL = "<?php echo base_url();?>"; </script>
-<link href="../../public/css/homepage.css" rel="stylesheet" type="text/css" />
+<link href="./public/css/homepage.css" rel="stylesheet" type="text/css" />
 
-<script src="../../public/js/homepage.js" ></script>
-<script src="../../public/js/jquery-1.11.2.min.js" ></script>
+<script src="./public/js/homepage.js" ></script>
+<script src="./public/js/jquery-1.11.2.min.js" ></script>
 
 <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
 
-<base href="<?php echo base_url();?>"/>
-<script type="text/javascript"> window.URL = "<?php echo base_url();?>";</script>
+
 <!-- 你最好检查一下这两行 -->
 
 </head>
@@ -22,8 +21,8 @@
 <div id="background">
 <div id="top">
 	<div id="head">
-		<img src="../../public/images/hust.png" alt="hust" height="60px" width="84.4px" display="inline-block">
-		<img src="../../public/images/ei.png" alt="ei" class="ei" height="60px" width="65.5px">
+		<img src="./public/images/hust.png" alt="hust" height="60px" width="84.4px" display="inline-block">
+		<img src="./public/images/ei.png" alt="ei" class="ei" height="60px" width="65.5px">
 		<div class="nav">
 			<div class="nav_cell"><a href = './index.php/show/index'>首页</a></div>
 			<div class="nav_cell"><a href = './index.php/show/articlelist'>文章</a></div>
